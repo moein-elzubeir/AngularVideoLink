a035569ccb8c340a822c0747cd1ea5f39fd4846f
moein-elzubeir <moein.elzubeir@gmail.com>
2020-07-17 02:04:41 +0200

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormGroup,  FormControl,  Validators } from '@angular/forms';
+import { CalendarService } from './calendar.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'VideoLinkApp';
+  inputForm: FormGroup
+  hangoutLink: string;
+
+  constructor(private calendarService: CalendarService) {
+    this.createForm();
+  }
+
+  createForm() {
+    this.inputForm = new FormGroup({
+      userA: new FormControl("", [Validators.required,
+            Validators.pattern('[a-zA-z0-9_\.]+@[a-zA-Z]+\.[a-zA-Z]+')]),
+      userB: new FormControl("",           [Validators.required,
+            Validators.pattern('[a-zA-z0-9_\.]+@[a-zA-Z]+\.[a-zA-Z]+')]),
+      date: new FormControl("", Validators.required),
+      time: new FormControl("", Validators.required)
+    });
+  }
+
+  createEvent(){
+    console.log(this.inputForm.value);
+
+    this.calendarService.getLink(this.inputForm.value).subscribe(
+      (response) => {
+        console.log(response['data']);
+        this.hangoutLink = response['data'];
+      }
+    );
+
+    this.inputForm.reset();
+  }
 }
---
+++ src/app/calendar.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse, HttpParams } from '@angular/common/http';
+
+import { Observable, throwError } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CalendarService {
+  baseUrl = 'http://localhost:8080/api';
+
+  constructor(private http: HttpClient) {}
+
+  getLink(details: object): Observable<Object> {
+    const options = {headers: {'Content-Type': 'application/json'}};
+    return this.http.post(`${this.baseUrl}/calendar`, details, options).pipe(catchError(this.handleError));
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    console.log(error);
+
+    // return an observable with a user friendly message
+    return throwError('Error! something went wrong.');
+  }
+}
